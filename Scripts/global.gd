8787446ff847da68329175b6046a7112ed033c6c
Pfftz <Kalixpfft+github@protonmail.com>
2024-11-11 10:52:23 +0700

    first commit wolfstain emulate

---
+++ Scripts/global.gd
@@ -0,0 +1,17 @@
+extends Node
+
+var ammo = 100
+var current_weapon
+
+func _ready():
+    current_weapon = "knife"
+
+func _process(delta):
+    if Input.is_action_just_pressed("knife"):
+        current_weapon = "knife"
+    elif Input.is_action_just_pressed("gun"):
+        current_weapon = "gun"
+    elif Input.is_action_just_pressed("minigun"):
+        current_weapon = "minigun"
+    elif Input.is_action_just_pressed("machinegun"):
+        current_weapon = "machinegun"
---
+++ Scripts/player.gd
@@ -0,0 +1,29 @@
+extends CharacterBody3D
+
+
+const SPEED = 5.0
+const TURN_SPEED = 0.05
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("left", "right", "up", "down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+		
+	if Input.is_action_pressed("left"):
+		self.rotate_y(TURN_SPEED)
+	if Input.is_action_pressed("right"):
+		self.rotate_y(-TURN_SPEED)
+
+	move_and_slide()
